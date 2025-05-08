c521755d2d3440e10b88473026b626f7d9caf7a9
riboli <lucasriboli57@gmail.com>
2025-05-08 12:05:53 -0300

    verify vsc

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -1,7 +1,15 @@
-yay -S --needed visual-studio-code-bin --noconfirm
+if ! command -v code &> /dev/null; then
+    echo "Visual Studio Code não encontrado. Instalando..."
+    yay -S --needed visual-studio-code-bin --noconfirm
+else
+    echo "Visual Studio Code já está instalado. Pulando a instalação."
+fi
 
 mkdir -p ~/.config/Code/User
 cp ~/.local/share/omakub/configs/vscode.json ~/.config/Code/User/settings.json
 
-# Install default supported themes
-code --install-extension enkia.tokyo-night
+if ! code --list-extensions | grep -q "enkia.tokyo-night"; then
+    code --install-extension enkia.tokyo-night
+else
+    echo "Tema 'enkia.tokyo-night' já está instalado."
+fi
